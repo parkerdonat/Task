5e55ee5f2f9da46507d1f65ec15bf1d0daa17a6b
Parker Donat <parkerdonat@gmail.com>
2016-04-13 14:54:56 -0600

    Finished Model Object and Basic Views

---
+++ Task/Task.swift
@@ -0,0 +1,65 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Parker Donat on 4/13/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+
+class Task: NSObject, NSCoding {
+    private let NameKey = "name"
+    private let NotesKey = "notes"
+    private let DueKey = "due"
+    private let CompleteKey = "isComplete"
+    
+    var name: String
+    var notes: String?
+    var due: NSDate?
+    var isComplete: Bool
+    
+    init(name: String, notes: String? = nil, due: NSDate? = nil) {
+        self.name = name
+        self.notes = notes
+        self.due = due
+        self.isComplete = false
+    }
+    
+    // MARK: - NSCODING
+    
+    @objc required init?(coder aDecoder: NSCoder) {
+        
+        guard let name = aDecoder.decodeObjectForKey(NameKey) as? String else { return nil }
+        
+        self.name = name
+        self.notes = aDecoder.decodeObjectForKey(NotesKey) as? String
+        self.due = aDecoder.decodeObjectForKey(DueKey) as? NSDate
+        self.isComplete = aDecoder.decodeBoolForKey(CompleteKey)
+    }
+    
+    @objc func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.name, forKey: NameKey)
+        aCoder.encodeObject(self.notes, forKey: NotesKey)
+        aCoder.encodeObject(self.due, forKey: DueKey)
+        aCoder.encodeBool(self.isComplete, forKey: CompleteKey)
+    }
+    
+    override func isEqual(object: AnyObject?) -> Bool {
+        if let rhs = object as? Task {
+            
+            return (self.name == rhs.name) && (self.notes == rhs.notes) && (self.isComplete == rhs.isComplete)
+        } else {
+            return false
+        }
+    }
+}
+
+// MARK: Equality
+
+func ==(lhs: Task, rhs: Task) -> Bool {
+    
+    return (lhs.name == rhs.name) && (lhs.notes == rhs.notes) && (lhs.isComplete == rhs.isComplete)
+    
+}
